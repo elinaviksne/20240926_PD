eac9be30fc52fdfc8622fdc2d4f001d23b565275
unknown <dummy@gmail.com>
2024-09-26 10:36:04 +0300

    Added the css for headings

--- web/blog.php
+++ web/blog.php
@@ -9,12 +9,12 @@
 </head>
 <body>
 
-    <h1>Tiešsaistes dienasgrāmata</h1>
+    <h1 class = "gold-virsraksts">Tiešsaistes dienasgrāmata</h1>
     <a href="index.php">Webinārs</a>
 
     <div class = "biography">
 
-        <h2>Biogrāfija</h2>
+        <h2 class = "small-heading">Biogrāfija</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, autem aperiam voluptate facere animi omnis corrupti sint ipsum qui obcaecati nemo quis rem? Corrupti nostrum expedita eveniet, tenetur explicabo, officiis, error id a earum fugiat quas! Porro, rem dolorum culpa, voluptatum neque molestiae eos, incidunt quod magni blanditiis voluptas est ratione. Natus, consequatur. Obcaecati ea voluptatibus vitae minus est similique optio modi sunt facilis voluptas animi placeat eaque aut deserunt ipsum laborum tenetur sequi in, adipisci, nesciunt necessitatibus laboriosam? Enim magni minus voluptatem illo. Nemo nesciunt quae commodi consectetur, magni consequatur velit ducimus atque enim, aspernatur inventore. Repellendus, animi quia.</p>
 
 
@@ -23,7 +23,7 @@
 
     <div class = "container">
 
-        <h2>Kāpēc es nolēmu kļūt par programmētaju?</h2>
+        <h2 class = "small-heading">Kāpēc es nolēmu kļūt par programmētaju?</h2>
         <ul>
             <li>Likās interesanti</li>
             <li>Dzīve jau bija saistīta ar datoru un IT</li>
