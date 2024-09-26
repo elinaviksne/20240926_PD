91bcfacf7f2750166cc8375425cbf585c2f5b8c8
unknown <dummy@gmail.com>
2024-09-26 10:35:41 +0300

    Added the form and js

--- web/index.php
+++ web/index.php
@@ -12,11 +12,39 @@
     <h1>Piesakies Webināram!</h1>
     <a href="blog.php">My Blog</a>
 
-    <form action="submit" method = "post">
+
+    <div class = "form-container">
+        <form action="submit" method = "post">
+            
+            <input type="text" value = "name">
+            <br>
+            <input type="text" value = "e-mail">
+            <br>
+            <select name="Course Level">
+                <option value="beginner">Beginner</option>
+                <option value="intermediate">Intermediate</option>
+                <option value="advanced">Advanced</option>
+            </select>
+
+            <br>
+
+            <p>Vai vēlies saņemt info par citiem mūsu organizētajiem pasākumiem?</p>
+            <label for="yes-rb">Yes</label>
+            <input type="radio" name="yes" id="yes-rb">
+            <label for="no-rb">No</label>
+            <input type="radio" name="no" id="no-rb">
 
 
-    </form>
+            <br>
+            <button type = "submit">Submit</button>
 
+
+        </form>
+    </div>
+    
+
+
+    <script src = "app.js"></script>
 
 </body>
 </html>
