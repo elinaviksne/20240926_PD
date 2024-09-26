a831572c5b1a4e47705ea1fa87743c98c5040b9b
unknown <dummy@gmail.com>
2024-09-26 10:07:19 +0300

    Linked css, added h1 and made an a tag to link index and blog

--- web/index.php
+++ web/index.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    
     <title>Webinārs</title>
 </head>
 <body>
+
     <h1>Piesakies Webināram!</h1>
+    <a href="blog.php">My Blog</a>
 
-    <form action="" method = "post">
+    <form action="submit" method = "post">
 
-    
+
     </form>
 
 
