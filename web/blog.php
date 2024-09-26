58d633f7c47752c45adfa8ed9a1a6e46706c2531
unknown <dummy@gmail.com>
2024-09-26 10:06:17 +0300

    Linked css, added biography and the 2nd part

--- web/blog.php
+++ web/blog.php
@@ -3,10 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    
     <title>Blog</title>
 </head>
 <body>
+
+    <h1>Tiešsaistes dienasgrāmata</h1>
+    <a href="index.php">Webinārs</a>
+
+    <div class = "biography">
+
+        <h2>Biogrāfija</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, autem aperiam voluptate facere animi omnis corrupti sint ipsum qui obcaecati nemo quis rem? Corrupti nostrum expedita eveniet, tenetur explicabo, officiis, error id a earum fugiat quas! Porro, rem dolorum culpa, voluptatum neque molestiae eos, incidunt quod magni blanditiis voluptas est ratione. Natus, consequatur. Obcaecati ea voluptatibus vitae minus est similique optio modi sunt facilis voluptas animi placeat eaque aut deserunt ipsum laborum tenetur sequi in, adipisci, nesciunt necessitatibus laboriosam? Enim magni minus voluptatem illo. Nemo nesciunt quae commodi consectetur, magni consequatur velit ducimus atque enim, aspernatur inventore. Repellendus, animi quia.</p>
+
+
+    </div>
     
+
+    <div class = "container">
+
+        <h2>Kāpēc es nolēmu kļūt par programmētaju?</h2>
+        <ul>
+            <li>Likās interesanti</li>
+            <li>Dzīve jau bija saistīta ar datoru un IT</li>
+            <li>Brālis ir programmētajs</li>
+            <li>Var dabūt labu algu</li>
+            <li>Var strādāt no mājām vai klātienē</li>
+        </ul>
+
+    </div>
     
+
+
 </body>
 </html>
